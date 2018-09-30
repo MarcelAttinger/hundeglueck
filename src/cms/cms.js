3e3a650a3b440709c938eb7142bcf181f93a1b75
mattinger <marcel.attinger@udg.de>
2018-09-30 19:59:57 +0200

    Develop Hard Bro

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,6 @@ import CMS from 'netlify-cms';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 
-CMS.registerPreviewStyle('/styles.css');
+CMS.registerPreviewStyle('../stylesheets/main.scss');
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
